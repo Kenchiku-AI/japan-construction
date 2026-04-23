0885d5fe4a4fe1d9f1347fef9335a3cd4f9150c3
therealcos <therealcos@therealcoss-MacBook-Pro.local>
2026-04-22 14:28:36 -1000

    fix health

--- app/core/config.py
+++ app/core/config.py
@@ -1,22 +1,21 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-  SECRET_KEY: str
-  ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-  REFRESH_TOKEN_EXPIRE_DAYS: int = 30
-  ALGORITHM: str = "HS256"
-  SUPER_USER_EMAIL: str
+  ENV: str
   DATABASE_URL: str
-  WEB_CLIENT_URL: str = "http://localhost:3000"
-  SECURE_COOKIE: bool = False # Set this to True in prod
+  SECRET_KEY: str
+  SUPER_USER_EMAIL: str
+  ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+  REFRESH_TOKEN_EXPIRE_DAYS: int = 7
   NO_REPLY_EMAIL: str
   SUPPORT_EMAIL: str
   OPENAI_API_KEY: str
-  AWS_ACCESS_KEY_ID: str
-  AWS_SECRET_ACCESS_KEY: str
   AWS_REGION: str
   S3_BUCKET: str
   SQS_QUEUE_URL: str
+  ALGORITHM: str = "HS256"
+  WEB_CLIENT_URL: str = "http://localhost:3000"
+  SECURE_COOKIE: bool = False # Set this to True in prod
   REDIS_URL: str = "redis://redis:6379"
 
   class Config:
