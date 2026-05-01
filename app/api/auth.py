a96af170b8a5c09b509e285f0ee2711ce54a4c79
therealcos <0xarowana@gmail.com>
2026-04-30 15:06:49 -1000

    fix company import

--- app/api/auth.py
+++ app/api/auth.py
@@ -19,6 +19,7 @@ from app.core.security import (
 )
 from app.db.models.refresh_token import RefreshToken
 from app.db.models.user import User
+from app.db.models.company import Company
 from app.db.models.invitation import Invitation
 from app.db.models.password_reset_token import PasswordResetToken
 from app.db.session import get_db
