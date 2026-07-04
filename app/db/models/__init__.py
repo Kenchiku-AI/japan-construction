3a028f8bf15379dd4ef363aec41cebc7b4a7fd5f
kenchikuai <alex@kenchiku.ai>
2026-07-04 23:01:15 +0900

    wip

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -7,7 +7,7 @@ from app.db.models.project import Project, ProjectStatus
 from app.db.models.project_guest_link import ProjectGuestLink
 from app.db.models.billing_plan import BillingPlan
 from app.db.models.line_message import LineMessage
-from app.db.models.work_item import WorkItem
+from app.db.models.action_item import ActionItem
 from app.db.models.report import (
   ReportParentType,
   ReportStatus,
